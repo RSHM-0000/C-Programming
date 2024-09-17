9072ef36ac77b3b0c07e4ff4633ac5614afa4ed1
RSHM-0000 <harinimahalakshmi.rs@gmail.com>
2024-09-17 10:31:48 +0530

    Update lab2_q14.c

--- lab2_q14.c
+++ lab2_q14.c
@@ -1,6 +1,3 @@
-/**
- * C program to check whether a triangle is valid or not if angles are given
- */
 
 #include <stdio.h>
 
@@ -8,18 +5,11 @@ int main()
 {
     int angle1, angle2, angle3, sum;
 
-    /* Input all three angles of triangle */
     printf("Enter three angles of triangle: \n");
     scanf("%d%d%d", &angle1, &angle2, &angle3);
 
-    /* Calculate sum of angles */
     sum = angle1 + angle2 + angle3; 
 
-    /*
-     * If sum of angles is 180 and
-     * angle1, angle2, angle3 is not 0 then
-     * triangle is valid.
-     */
     if(sum == 180 && angle1 > 0 && angle2 > 0 && angle3 > 0) 
     {
         printf("Triangle is valid.");

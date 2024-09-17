c8e501e52a9ff3f887dc29aa2f2aa294005b45c8
RSHM-0000 <harinimahalakshmi.rs@gmail.com>
2024-09-17 10:34:52 +0530

    lab2_q18.c

--- lab2_q18.c
+++ lab2_q18.c
@@ -1,14 +1,9 @@
-/**
- * C program to calculate profit or loss
- */
-
 #include <stdio.h>
 
 int main()
 {
     int cp,sp, amt; 
-    
-    /* Input cost price and selling price of a product */
+
     printf("Enter cost price: ");
     scanf("%d", &cp);
     printf("Enter selling price: ");
@@ -16,19 +11,17 @@ int main()
     
     if(sp > cp)
     {
-        /* Calculate Profit */
         amt = sp - cp;
         printf("Profit = %d", amt);
     }
     else if(cp > sp)
     {
-        /* Calculate Loss */
+
         amt = cp - sp;
         printf("Loss = %d", amt);
     }
     else
     {
-        /* Neither profit nor loss */
         printf("No Profit No Loss.");
     }
 

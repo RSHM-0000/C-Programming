37f49ebef2e4b47b3193b4763fa285da8e54a46d
RSHM-0000 <harinimahalakshmi.rs@gmail.com>
2024-09-17 10:35:26 +0530

    lab2_q19.c

--- lab2_q19.c
+++ lab2_q19.c
@@ -1,7 +1,3 @@
-/**
- * C program to enter marks of five subjects and find percentage and grade
- */
-
 #include <stdio.h>
 
 int main()
@@ -9,18 +5,14 @@ int main()
     int phy, chem, bio, math, comp; 
     float per; 
 
-    /* Input marks of five subjects from user */
     printf("Enter five subjects marks: ");
     scanf("%d%d%d%d%d", &phy, &chem, &bio, &math, &comp);
 
-
-    /* Calculate percentage */
     per = (phy + chem + bio + math + comp) / 5.0;
 
     printf("Percentage = %.2f\n", per);
 
 
-    /* Find grade according to the percentage */
     if(per >= 90)
     {
         printf("Grade A");
